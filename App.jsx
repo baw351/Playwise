abb536bbcb130cd1de51479ecb0e47aaf241d39b
baw351 <72230455+baw351@users.noreply.github.com>
2025-04-09 12:04:52 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import SearchBar from './components/SearchBar';
+
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center' }}>Playwise </h1>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
